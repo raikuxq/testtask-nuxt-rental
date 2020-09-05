a5fc7fd102b8234b3754f52b7f6bf572786c5387
Alexey Stepanov <Raddosx@gmail.com>
2020-09-05 21:00:38 +0300

    Add Page product details

---
+++ pages/products/_id/specifications.vue
@@ -0,0 +1,65 @@
+<template>
+  <div :class="$style.specifications">
+    <div :class="$style.description">
+      <p>{{ specificationsDescription }}</p>
+    </div>
+
+    <div :class="$style.features" v-if="hasSpecifications">
+      <h2>Features:</h2>
+
+      <div
+        :class="$style.featuresItem"
+        v-for="feature in specificationsFeatures"
+        :key="feature.title"
+      >
+        <feature
+          :title="feature.title"
+          :description="feature.description"
+          :icon="feature.icon"
+        />
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import Feature from '@/components/Feature/Feature'
+
+export default {
+  name: 'SubRouteSpecifications',
+  components: {
+    Feature
+  },
+  props: {
+    specificationsDescription: {
+      type: String,
+      required: false,
+      default: ''
+    },
+    specificationsFeatures: {
+      type: Array || null,
+      required: false,
+      default: null
+    }
+  },
+  computed: {
+    hasSpecifications () {
+      return this.specificationsFeatures?.length > 0
+    }
+  }
+}
+</script>
+
+<style lang="scss" module>
+@import "~@/assets/style/base/typography.scss";
+@import "~@/assets/style/variables/varibales.scss";
+
+.description {
+  margin-bottom: $offset-400;
+}
+
+.featuresItem {
+  margin-bottom: $offset-300;
+}
+
+</style>
---
+++ pages/products/_id/terms.vue
@@ -0,0 +1,42 @@
+<template>
+  <div :class="$style.terms">
+    <div :class="$style.description">
+      <p>{{ termsDescription }}</p>
+    </div>
+
+    <div :class="$style.conditions" v-if="termsConditions.length">
+      <h2>Additional conditions:</h2>
+      <ul>
+        <li v-for="condition in termsConditions">
+          {{ condition }}
+        </li>
+      </ul>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'SubRouteTerms',
+  props: {
+    termsDescription: {
+      type: String,
+      required: true
+    },
+    termsConditions: {
+      type: Array || null,
+      required: false,
+      default: null
+    }
+  }
+}
+</script>
+
+<style lang="scss" module>
+@import "~@/assets/style/base/typography.scss";
+@import "~@/assets/style/variables/varibales.scss";
+
+.description {
+  margin-bottom: $offset-400;
+}
+</style>
