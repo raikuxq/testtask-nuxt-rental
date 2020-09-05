4794c8c9b939fba1e38a03f1d98c8726e5326f0e
Alexey Stepanov <Raddosx@gmail.com>
2020-09-05 19:53:06 +0300

    Add modals manager component

---
+++ components/_app/AppModalsManager.vue
@@ -0,0 +1,30 @@
+<template>
+  <div :class="$style.modalsContainer">
+    <form-add-vehicle v-if="isFormAddVehicleActive" />
+  </div>
+</template>
+
+<script>
+import { mapGetters } from 'vuex'
+import FormAddVehicle from '@/components/_forms/FormAddVehicle'
+
+export default {
+  name: 'AppModalsContainer',
+  components: {
+    FormAddVehicle
+  },
+  computed: {
+    ...mapGetters('modals', [
+      'isAnyModalActive',
+      'isFormAddVehicleActive'
+    ])
+  }
+}
+</script>
+
+<style lang="scss" module>
+.modalsContainer {
+  position: relative;
+  height: 100%;
+}
+</style>
