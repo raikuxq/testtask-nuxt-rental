17f626550cef015d469b34b6b9f7ab2fd5aeda89
Alexey Stepanov <Raddosx@gmail.com>
2020-09-06 15:10:17 +0300

    Add image lazy placeholder

--- plugins/vue-lazyload.js
+++ plugins/vue-lazyload.js
@@ -1,7 +1,10 @@
 import Vue from 'vue'
 import VueLazyload from 'vue-lazyload'
 
+import picture from '@/assets/sprite/svg/placeholder.svg'
+
 Vue.use(VueLazyload, {
   preLoad: 1,
-  attempt: 1
+  attempt: 1,
+  loading: picture
 })
