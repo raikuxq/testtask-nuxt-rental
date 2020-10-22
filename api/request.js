0f696fb8bccf8817cb9b37127c01a30336fc9f59
Alexey Stepanov <Raddosx@gmail.com>
2020-10-22 22:08:20 +0300

    Remove server fake delay

--- api/request.js
+++ api/request.js
@@ -3,8 +3,5 @@ import vehicles from '@/api/vehicles.json'
 /** Emulate request */
 export const getVehicles = () =>
   new Promise((resolve, reject) => {
-    const delay = parseInt(Math.random() * 1000)
-    setTimeout(() => {
-      resolve(vehicles)
-    }, delay)
+    resolve(vehicles)
   })
